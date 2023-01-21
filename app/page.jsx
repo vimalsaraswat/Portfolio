7a066cff089efaac71eb68f0a9cd00717c660baa
vimalsaraswat <saraswatvimal1@gmail.com>
2023-01-21 10:39:49 +0000

    add sections in page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,61 @@
-export default function Home() {
+export default function Page() {
   return (
     <>
+    <Header />
+    <Main />
+    <Footer />
     </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+function Main() {
+  return (
+    <main>
+      <Home />
+      <Skills />
+      <Projects />
+      <About />
+    </main>
+  );
+}
+function Footer() {
+  return (
+    <footer>
+
+    </footer>
+  );
+}
+
+function Home() {
+  return (
+    <section>
+      <h1>Vimal Saraswat</h1>
+    </section>
+  );
+}
+function Skills() {
+  return (
+    <section></section>
+  );
+}
+function Projects() {
+  return (
+    <section></section>
+  );
+}
+function About() {
+  return (
+    <section></section> 
   );
 }
